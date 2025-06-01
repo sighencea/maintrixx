861554d12d691b787d5692ae8d976347ad9b4d6c
sighencea <55688562+sighencea@users.noreply.github.com>
2025-06-01 08:02:08 +0100

    Update supabase-config.example.js

--- js/supabase-config.example.js
+++ js/supabase-config.example.js
@@ -3,7 +3,7 @@
 // You can get these from your Supabase project settings > API.
 // IMPORTANT: Make sure supabase-config.js is added to your .gitignore file and is NOT committed to your repository.
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL_HERE';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://njubyuqpavmxqdokxftq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdWJ5dXFwYXZteHFkb2t4ZnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNjQxMTAsImV4cCI6MjA2Mzg0MDExMH0.LAoiywXkz-Cx9Y178j_YsPu8y7mETWN53tN4jKHo2Tw';
 
 export { SUPABASE_URL, SUPABASE_ANON_KEY };
